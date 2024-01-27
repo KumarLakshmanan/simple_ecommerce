8fb88dff29d2b1999006a178341aa62244b0ebd2
Lakshmanan R <klakshmanan48@gmail.com>
2024-01-27 16:52:51 +0530

    added user page

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>E Commerce</title>
+		<meta charset="utf-8" />
+		<meta
+			name="viewport"
+			content="width=device-width, initial-scale=1, shrink-to-fit=no"
+		/>
+
+		<link
+			href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,700"
+			rel="stylesheet"
+			type="text/css"
+		/>
+
+		<link
+			rel="stylesheet"
+			href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+		/>
+
+		<link rel="stylesheet" href="css/style.css" />
+	</head>
+	<body>
+		<nav
+			class="navbar navbar-expand-lg ftco_navbar ftco-navbar-light"
+			id="ftco-navbar"
+		>
+			<div class="container">
+				<a class="navbar-brand" href="index.html">E Commerce</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#ftco-nav"
+					aria-controls="ftco-nav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="fa fa-bars"></span> Menu
+				</button>
+				<div class="collapse navbar-collapse" id="ftco-nav">
+					<ul class="navbar-nav ml-auto mr-md-3">
+						<li class="nav-item">
+							<a href="#" class="nav-link">Page One</a>
+						</li>
+						<li class="nav-item"><a href="#" class="nav-link">Page Two</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">Page Three</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/popper.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
